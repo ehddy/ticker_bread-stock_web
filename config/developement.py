426d2fb405697f414925bec801e45abf3aca8446
ehddy <rlxk751@naver.com>
2023-11-26 10:15:27 +0900

    postgresql 주소 삭제

--- config/developement.py
+++ config/developement.py
@@ -3,7 +3,7 @@ from config.default import *
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 
-# SQLALCHEMY_DATABASE_URI = 'postgresql://ticker_bread:ticker_bread91@postgreSQL:5432/stock' 
+# SQLALCHEMY_DATABASE_URI = 'postgresql://@postgreSQL:5432/stock' 
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
