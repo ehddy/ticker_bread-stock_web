6d2029009fa1f86cb4fcd052009ff7106451147f
wonse1111 <wonse1111@naver.com>
2023-12-10 15:03:13 +0000

    get_kospikosdaq.py

--- ticker_bread/modules/get_kospikosdaq.py
+++ ticker_bread/modules/get_kospikosdaq.py
@@ -1,62 +1,46 @@
 import pandas as pd
-import psycopg2
-import psycopg2.extras as extras
-from psycopg2.extras import RealDictCursor
-import yaml
-import numpy as np
+import FinanceDataReader as fdr
+import requests as rq
+from bs4 import BeautifulSoup as bs
 
-import requests
-import datetime
-
-class Database:
+class StockData:
     def __init__(self):
-        # db 정보
-        with open('/home/code/stock_git/ticker_bread-stock_web/ticker_bread/modules/db_info.yml', encoding='UTF-8') as f:
-            _cfg = yaml.load(f, Loader=yaml.FullLoader)
+        self.recent_date = self.get_recent_date()
 
-        # PostgreSQL 연결 정보
-        self.conn_info = {
-            "host": _cfg['POSTGRES_HOST'],
-            "database": _cfg['POSTGRES_DB'],
-            "port":"5432",
-            "user": _cfg['POSTGRES_USER'],
-            "password": _cfg['POSTGRES_PASSWORD']
-        }
+    # 최근 장 마감일자 불러오기
+    def get_recent_date(self):
+        url = 'https://finance.naver.com/sise/'
+        res = rq.get(url)
+        html = bs(res.content, 'html.parser')
+        recent_date = html.select_one('#time3').text.strip().split('장')[0].replace('.', '')
+        recent_date = recent_date[:4] + '-' + recent_date[4:6] + '-' + recent_date[6:8]
+        return recent_date
 
-        # PostgreSQL 연결
-        self.conn = psycopg2.connect(**self.conn_info)
+    # 최근 장 마감일자 기준 코스피, 코스닥 정보 호출 (지수, 증감률, 증감액)
+    def get_recent_index_data(self, index):
+        df = fdr.DataReader(index, end=self.recent_date)
+        df['ChangeAmount'] = df['Close'].diff()
+        recent_data = df.iloc[-1][['Close', 'Change', 'ChangeAmount']].to_frame().T
+        # 증감률을 퍼센트로 변환하고 부호, '%' 추가
+        recent_data['Change'] = recent_data['Change'].apply(lambda x: '+' + str(round(x * 100, 2)) + '%' if x > 0 else str(round(x * 100, 2)) + '%')
+        # 증감액에 부호 추가하고 소수점 둘째자리까지 표시
+        recent_data['ChangeAmount'] = recent_data['ChangeAmount'].apply(lambda x: '+' + str(round(x, 2)) if x > 0 else str(round(x, 2)))
+        return recent_data
 
-    def get_index_info(self, index_name, end_date):
-        # 지수 정보 가져오기
-        query = f'''
-        SELECT keystat_name, data_value, cycle_date
-        FROM economic_indicators
-        WHERE keystat_name = '{index_name}' AND cycle_date <= '{end_date}'
-        ORDER BY cycle_date DESC
-        LIMIT 2;
-        '''
-        df = pd.read_sql(query, self.conn)
-    
-        # 계산
-        if len(df) < 2:
-            print('데이터가 충분하지 않습니다.')
-            return None
+    # 코스피, 코스닥 지표 시계열 데이터프레임 리턴
+    def get_index_data_over_time(self, index, start_date, freq='D'): # 'D'(일별), 'W'(주별), 'M'(월별), 'Y'(연별) 
+        df = fdr.DataReader(index, start_date, self.recent_date)
+        df = df.asfreq(freq, method='pad')
+        return df[['Close']]
 
-        index_value = df.loc[0, 'data_value']
-        change_amount = index_value - df.loc[1, 'data_value']
-        change_rate = change_amount / df.loc[1, 'data_value'] * 100
-    
-        return index_value, change_rate, change_amount
+stock_data = StockData()
 
-# 코스피 지수 조회
-db = Database()
-kospi_value, kospi_rate, kospi_amount = db.get_index_info('코스피지수', '2023-12-03')
-print('코스피 지수:', kospi_value)
-print('코스피 증감률:', kospi_rate)
-print('코스피 증감액:', kospi_amount)
+recent_kospi_df = stock_data.get_recent_index_data('KS11')
+recent_kosdaq_df = stock_data.get_recent_index_data('KQ11')
+# print(recent_kospi_df)
+# print(recent_kosdaq_df)
 
-# 코스닥 지수 조회
-kosdaq_value, kosdaq_rate, kosdaq_amount = db.get_index_info('코스닥지수', '2023-12-03')
-print('코스닥 지수:', kosdaq_value)
-print('코스닥 증감률:', kosdaq_rate)
-print('코스닥 증감액:', kosdaq_amount)
+# kospi_time_series_df = stock_data.get_index_data_over_time('KS11', '2023-01-01', 'D')
+# kosdaq_time_series_df = stock_data.get_index_data_over_time('KQ11', '2023-01-01', 'D')
+# print(kospi_time_series_df)
+# print(kosdaq_time_series_df)
